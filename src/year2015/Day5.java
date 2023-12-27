96c18fc201f5ac4474534c4271f34a601a125413
predicateCody <codypalmer1p18@gmail.com>
2023-12-27 05:28:15 +0000

    save point for Day 5

--- src/year2015/Day5.java
+++ src/year2015/Day5.java
@@ -4,50 +4,64 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class Day5 {
-
-    // properties
-    private static final String[] VOWELS = {"a", "e", "i", "o", "u"};
+    private static final char[] VOWELS = {'a', 'e', 'i', 'o', 'u'};
     private static final String[] REPEATED_CHARACTERS =
             {"aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn",
                     "oo", "pp", "qq", "rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz"};
-    private static final String[] NAUGHTY_PROPERTIES = {"ab", "cd", "pq", "xy",};
+    private static final String[] NAUGHTY_PROPERTIES = {"ab", "cd", "pq", "xy"};
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner readFile = new Scanner(new File("resources/2015/day5-inputs.txt"));
-
         ArrayList<String> santasList = new ArrayList<>();
 
         // Writing all the strings on Santa's list!
         while (readFile.hasNext()) {
             santasList.add(readFile.nextLine());
         }
-        ArrayList<String> vowelDetectionList = arrayFilter(santasList, VOWELS); // gets all vowels
-        ArrayList<String> repeatedDetectionList = arrayFilter(vowelDetectionList, REPEATED_CHARACTERS); // gets all repeated characters in the vowels
-        ArrayList<String> naughtyDetectionList = arrayFilter(repeatedDetectionList, NAUGHTY_PROPERTIES); // gets all naughties
-
-
-        System.out.println(repeatedDetectionList.size() - naughtyDetectionList.size());
-
+        ArrayList<String> filteredList = filteredList(santasList);
+        System.out.println(filteredList.size());
     }
 
-    private static ArrayList<String> arrayFilter(ArrayList<String> arr1, String[] arr2) {
-        ArrayList<String> arr = new ArrayList<>();
-        for (int i = 0; i < arr1.size(); i++) {
-            String line = arr1.get(i);
-            for (String index : arr2) {
-                Pattern p = Pattern.compile(index);
-                Matcher m = p.matcher(line);
+    private static ArrayList<String> filteredList(ArrayList<String> list) {
+        ArrayList<String> filtered = new ArrayList<>();
+        for (String line : list) {
 
-                if (m.find()) {
-                    arr.add(line);
+            // booleans for each of the properties
+            boolean hasVowels = hasVowels(line);
+            boolean hasRepeats = hasRepeatedOrNaughty(line, REPEATED_CHARACTERS);
+            boolean hasNaughty = hasRepeatedOrNaughty(line, NAUGHTY_PROPERTIES);
+
+            // checking whether a string satisfies these prerequisites
+            if (hasVowels && hasRepeats && !hasNaughty) {
+                filtered.add(line);
+            }
+        }
+        return filtered;
+    }
+
+
+    private static boolean hasVowels(String line) {
+        int vowelCount = 0;
+        for (char c : line.toCharArray()) {
+            for (char vowel : VOWELS) {
+                if (c == vowel) {
+                    vowelCount++;
                     break;
                 }
             }
         }
-        return arr;
+        return vowelCount >= 3; // Checking if there are at least three vowels
+    }
+
+    public static boolean hasRepeatedOrNaughty(String line, String[] arr) {
+        for (String pattern : arr) {
+            if (line.contains(pattern)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
